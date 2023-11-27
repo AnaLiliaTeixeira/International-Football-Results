6be032a45ffaa38d7e859631e1715cdc67687df4
Daniel Lopes <danielgoncalo2002@gmail.com>
2023-11-27 00:56:27 +0000

    Foram unido o trabalho da TP, feito em mongo e SQL.

--- BDA_project/project.py
+++ BDA_project/project.py
@@ -44,3 +44,109 @@ team = team.to_dict()
 teams.insert_many([{' tema' :team.values()}])
 
 client.close()
+
+############################SQL######################
+
+
+import mysql.connector
+import pandas as pd
+import json
+from pymongo import MongoClient
+
+# Create Connection
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password='senha123',
+    database = 'BDA2324_4'
+)
+
+mycursor = mydb.cursor()
+#mycursor.execute("CREATE DATABASE BDA2324_4")
+
+mycursor.execute("DROP TABLE IF EXISTS  Shootouts") 
+mycursor.execute("DROP TABLE IF EXISTS Goalscorers")
+mycursor.execute("DROP TABLE IF EXISTS Matches")
+mycursor.execute("DROP TABLE IF EXISTS Teams") 
+mycursor.execute("DROP TABLE IF EXISTS Tournaments")
+mycursor.execute("DROP TABLE IF EXISTS Countries") 
+
+mycursor.execute('''CREATE TABLE Countries (
+        country_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        country_name VARCHAR(245)
+    )
+''')
+
+mycursor.execute('''CREATE TABLE Tournaments(
+        tournament_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        tournament_name VARCHAR(245)
+    )
+''')
+
+mycursor.execute('''CREATE TABLE Teams (
+        team_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        team_name VARCHAR(245)
+    )
+''')
+
+mycursor.execute('''CREATE TABLE Matches (
+        match_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        date DATE,
+        home_team_id INTEGER,
+        away_team_id INTEGER,
+        home_score INTEGER,
+        away_score INTEGER,
+        tournament_id INTEGER,
+        city VARCHAR(245),
+        country_id INTEGER,
+        neutral BOOLEAN,
+        FOREIGN KEY(home_team_id) REFERENCES Teams(team_id),
+        FOREIGN KEY(away_team_id) REFERENCES Teams(team_id),
+        FOREIGN KEY(tournament_id) REFERENCES Tournaments(tournament_id),
+        FOREIGN KEY(country_id) REFERENCES Countries(country_id)
+    )
+''')
+
+mycursor.execute('''CREATE TABLE Shootouts (
+        shootout_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        match_id INTEGER,
+        winner_id INTEGER,
+        first_shooter_id INTEGER,
+        FOREIGN KEY(match_id) REFERENCES Matches(match_id),
+        FOREIGN KEY(winner_id) REFERENCES Teams(team_id),
+        FOREIGN KEY(first_shooter_id) REFERENCES Teams(team_id)
+    )
+''')
+
+mycursor.execute('''CREATE TABLE Goalscorers (
+        goal_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        match_id INTEGER,
+        team_id INTEGER,
+        scorer VARCHAR(245),
+        own_goal BOOLEAN,
+        penalty BOOLEAN,
+        FOREIGN KEY(match_id) REFERENCES Matches(match_id),
+        FOREIGN KEY(team_id) REFERENCES Teams(team_id)
+    )
+''')
+
+unique_countries = df_results['country'].unique()
+
+sql = "INSERT INTO countries (country_name) VALUES (%s)"
+for country in unique_countries:
+    val = (country,)
+    mycursor.execute(sql, val)
+mydb.commit()
+
+
+unique_teams =  df_shootouts['home_team'].tolist() + df_results['away_team'].tolist()
+unique_teams_set = set(unique_teams)
+unique_teams_list = list(unique_teams_set)
+print(len(unique_teams_list))
+
+sql = "INSERT INTO teams (team_name) VALUES (%s)"
+for team in unique_teams_list:
+    val = (team,)
+    mycursor.execute(sql, val)
+mydb.commit()
+
