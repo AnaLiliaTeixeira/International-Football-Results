05a2041a16632617d41ffcc36e5b17b8d748b6e3
Daniel Lopes <danielgoncalo2002@gmail.com>
2023-12-01 13:25:29 +0000

    Melhoramento de performance da criação da base de dados

--- BDA_project/mysql_creation.py
+++ BDA_project/mysql_creation.py
@@ -109,32 +109,34 @@ print("Table Goalscorers created!")
 
 #INSERT COUNTRIES VALUES
 unique_countries = df_results['country'].unique()
+unique_countries_list = [(country,) for country in unique_countries]
 sql = "INSERT INTO countries (country_name) VALUES (%s)"
-country_id_mapping = {}
-for country in unique_countries:
-    val = (country,)
-    mycursor.execute(sql, val) 
-    mydb.commit()
-    country_id = mycursor.lastrowid
-    country_id_mapping[country] = country_id
-
-print("The values of countries were inserted!")
-df_results['country'] = df_results['country'].map(country_id_mapping)
+mycursor.executemany(sql,unique_countries_list)
+mydb.commit()
+print("The values of Countries were inserted!")
+query = "SELECT * FROM countries"
+mycursor.execute(query)
+country_data = mycursor.fetchall()
+country_id_mapping = {country_id: country_name for country_id, country_name in country_data}
+df_results['country'] = df_results['country'].map({country_name: country_id for country_id, country_name in country_id_mapping.items()})
 df_results = df_results.rename(columns={'country': 'country_id'})
+
 
 #INSERT TEAMS VALUES
 unique_teams =  df_results['home_team'].tolist() + df_results['away_team'].tolist()
 unique_teams_set = set(unique_teams)
 unique_teams_list = list(unique_teams_set)
-team_id_mapping = {}
+team_data = [(team,) for team in unique_teams_list]
 sql = "INSERT INTO teams (team_name) VALUES (%s)"
-for team in unique_teams_list:
-    val = (team,)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    team_id = mycursor.lastrowid
-    team_id_mapping[team] = team_id
-print("The values of teams were inserted!")
+mycursor.executemany(sql,team_data)
+mydb.commit()
+query = "SELECT * FROM teams"
+mycursor.execute(query)
+team_data = mycursor.fetchall()
+team_id_mapping = {team_name: team_id for team_id, team_name in team_data}
+print("The values of Teams were inserted!")
+
+
 df_results['home_team'] = df_results['home_team'].map(team_id_mapping)
 df_results = df_results.rename(columns={'home_team': 'home_team_id'})
 df_results['away_team'] = df_results['away_team'].map(team_id_mapping)
@@ -146,7 +148,7 @@ df_goalscores = df_goalscores.rename(columns={'away_team': 'away_team_id'})
 df_goalscores['team'] = df_goalscores['team'].map(team_id_mapping)
 df_goalscores = df_goalscores.rename(columns={'team': 'team_id'})
 df_shootouts['home_team'] = df_shootouts['home_team'].map(team_id_mapping)
-df_shootouts = df_shootouts.rename(columns={'home_team': 'home_team'})
+df_shootouts = df_shootouts.rename(columns={'home_team': 'home_team_id'})
 df_shootouts['away_team'] = df_shootouts['away_team'].map(team_id_mapping)
 df_shootouts = df_shootouts.rename(columns={'away_team': 'away_team_id'})
 df_shootouts['winner'] = df_shootouts['winner'].map(team_id_mapping)
@@ -157,56 +159,58 @@ df_shootouts = df_shootouts.rename(columns={'first_shooter': 'first_shooter_id'}
 
 #INSERT TOURNAMENTS VALUES
 unique_tournaments=  df_results['tournament'].unique()
+tournment_data = [(tournament,) for tournament in unique_tournaments]
 sql = "INSERT INTO tournaments (tournament_name) VALUES (%s)"
-tournament_id_mapping = {}
-for tournament in unique_tournaments:
-    val = (tournament,)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    tournament_id = mycursor.lastrowid
-    tournament_id_mapping[tournament] = tournament_id
+mycursor.executemany(sql,tournment_data)
+mydb.commit()
+query = "SELECT * FROM tournaments"
+mycursor.execute(query)
+tournment_data = mycursor.fetchall()
+tournament_id_mapping = {tournament_name: tournament_id for tournament_id, tournament_name in tournment_data}
+print("The values of tournaments were inserted!")
 df_results['tournament'] = df_results['tournament'].map(tournament_id_mapping)
 df_results = df_results.rename(columns={'tournament': 'tournament_id'})
-print("The values of tournaments were inserted!")
 
+
+#INSERT MATCHES VALUES
 df_results_list = df_results.values.tolist()
-# matches_insert_query = "INSERT INTO Matches (date, home_team_id, away_team_id, home_score, away_score, tournament_id, city, country_id, neutral) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-# mycursor.executemany(matches_insert_query, df_results_list)
-# mydb.commit()
-match_id_mapping = {}
-for match in df_results_list:
-    matches_insert_query = "INSERT INTO Matches (date, home_team_id, away_team_id, home_score, away_score, tournament_id, city, country_id, neutral) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    mycursor.execute(matches_insert_query,match)
-    mydb.commit()
-    match_id = mycursor.lastrowid
-    unique_key = (match[0], match[1], match[2])
-    match_id_mapping[unique_key] = match_id
+matches_insert_query = "INSERT INTO Matches (date, home_team_id, away_team_id, home_score, away_score, tournament_id, city, country_id, neutral) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+mycursor.executemany(matches_insert_query, df_results_list)
+mydb.commit()
 print("The values of Matches were inserted!")
+query = "SELECT match_id,date,home_team_id,away_team_id FROM Matches"
+mycursor.execute(query)
+matchs_data = mycursor.fetchall()
+match_id_mapping = {(date,home_team_id,away_team_id): match_id for match_id, date,home_team_id,away_team_id in matchs_data}
 
-# Inserção na tabela Shootouts
+
+
+
+#INSERT SHOOTOUTS VALUES
+df_shootouts['date'] = pd.to_datetime(df_shootouts['date']).dt.date
+df_shootouts['match_id'] = df_shootouts.apply(lambda row: match_id_mapping.get((row['date'], row['home_team_id'], row['away_team_id'])), axis=1)
+colunas_para_remover = ['date', 'home_team_id', 'away_team_id']
+df_shootouts = df_shootouts.drop(colunas_para_remover, axis=1)
+colunas = list(df_shootouts.columns)
+colunas = ['match_id'] + [coluna for coluna in colunas if coluna != 'match_id']
+df_shootouts = df_shootouts[colunas]
 df_shootouts_list = df_shootouts.values.tolist()
-for shootout in df_shootouts_list:
-    unique_key = (shootout[0], shootout[1], shootout[2])
-    match_id = match_id_mapping.get(unique_key)
-    if match_id is not None:
-        shootouts_insert_query = "INSERT INTO Shootouts (match_id, winner_id, first_shooter_id) VALUES (%s, %s, %s)"
-        shootout_data = (match_id, shootout[3], shootout[4])
-        mycursor.execute(shootouts_insert_query, shootout_data)
-        mydb.commit()
-    else:
-        print(shootout)
+shootouts_insert_query = "INSERT INTO Shootouts (match_id, winner_id, first_shooter_id) VALUES (%s, %s, %s)"
+mycursor.executemany(shootouts_insert_query, df_shootouts_list)
+mydb.commit()
 print("The values of Shootouts were inserted!")
 
+
 # Inserção na tabela Goalscorers
+df_goalscores['date'] = pd.to_datetime(df_goalscores['date']).dt.date
+df_goalscores['match_id'] = df_goalscores.apply(lambda row: match_id_mapping.get((row['date'], row['home_team_id'], row['away_team_id'])), axis=1)
+colunas_para_remover = ['date', 'home_team_id', 'away_team_id']
+df_goalscores = df_goalscores.drop(colunas_para_remover, axis=1)
+colunas = list(df_goalscores.columns)
+colunas = ['match_id'] + [coluna for coluna in colunas if coluna != 'match_id']
+df_goalscores = df_goalscores[colunas]
 df_goalscores_list = df_goalscores.values.tolist()
-for goalscore in df_goalscores_list:
-    unique_key = (goalscore[0], goalscore[1], goalscore[2])
-    match_id = match_id_mapping.get(unique_key)
-    if match_id is not None:
-        goalscorers_insert_query = "INSERT INTO Goalscorers (match_id, team_id, scorer, minute, own_goal, penalty) VALUES (%s, %s, %s, %s, %s, %s)"
-        goalscore_data = (match_id, goalscore[3], goalscore[4], goalscore[5], goalscore[6], goalscore[7])
-        mycursor.execute(goalscorers_insert_query, goalscore_data)
-        mydb.commit()
-    else:
-        print(shootout)
+goalscorers_insert_query = "INSERT INTO Goalscorers (match_id, team_id, scorer, minute, own_goal, penalty) VALUES (%s, %s, %s, %s, %s, %s)" 
+mycursor.executemany(goalscorers_insert_query, df_goalscores_list)
+mydb.commit()
 print("The values of Goalscorers were inserted!")
