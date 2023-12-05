f38ed83bd64e3b53c8b25714c7e49cfe7dde45fd
MiguelRamos1 <126819817+MiguelRamos1@users.noreply.github.com>
2023-12-05 17:58:40 +0000

    Update mongo_querys.py

--- BDA_project/mongo_querys.py
+++ BDA_project/mongo_querys.py
@@ -19,14 +19,20 @@ tournaments = db.tournaments
 simpleQuery1 = { '$and': [ { 'city': "Lisbon" }, {'date': {"$regex": "2023"}} ] }
 mydoc = matches.find(simpleQuery1)
 print("Matches in Lisbon in 2023:")
+counter=0
 for doc in mydoc:
-    pprint.pprint(doc)
+#    pprint.pprint(doc)
+    counter=counter+1
+print(counter)
 
 simpleQuery2 = {'$and':[{'scorer': 'Cristiano Ronaldo'}, {'minute': {'$lt': 5}}]}
 mydoc = goalscores.find(simpleQuery2)
 print("\nGolos que o Ronaldo marcou antes dos 5 minutos:")
+counter=0
 for doc in mydoc:
-    pprint.pprint(doc)
+#    pprint.pprint(doc)
+    counter=counter+1
+print(counter)
 # b. Two complex queries, using joins and aggregates, involving at least 2 tables/collections of your database (em que uma tem que ter mais do 5 joins)
 
 # aggregate the results for two queries: find all the scorer's names starting with A and count the repeats
@@ -35,8 +41,7 @@ for doc in mydoc:
 # mydoc= goalscores.aggregate([{ "$match": { "scorer": {"$regex": "^A"} } },
 #                              {"$group" : {"_id" : "$scorer", "count repeats" : {"$sum" : 1} }}])
             
-for doc in mydoc:
-    pprint.pprint(doc)
+
 
 #COMPLEX QUERY1: Top 5 jogadores (scorer), de Portugal(team), que tem mais golos no torneio: FIFA World Cup Qualification, 
 # cujo os jogos foram realizados em Portugal 
@@ -45,133 +50,119 @@ for doc in mydoc:
 complexQuery1 = [
     {
         "$lookup": {
-            "from": "Matches",
-            "localField": "_id",
-            "foreignField": "match._id",
+            "from": "matches",
+            "localField": "match_id",
+            "foreignField": "_id",
             "as": "match"
         }
     },
     {
-        "$unwind": "$match"
-    },
-    {
         "$lookup": {
-            "from": "Shootouts",
-            "localField": "shootouts._id",
-            "foreignField": "match._id",
-            "as": "shootout"
-        }
-    },
-    {
-        "$lookup": {
-            "from": "Teams",
-            "localField": "team._id",
+            "from": "teams",
+            "localField": "team_id",
             "foreignField": "_id",
             "as": "team"
         }
     },
     {
         "$lookup": {
-            "from": "Tournaments",
-            "localField": "_id",
-            "foreignField": "match.tournament_id",
+            "from": "tournaments",
+            "localField": "match.tournament_id",
+            "foreignField": "_id",
             "as": "tournament"
         }
     },
     {
         "$lookup": {
-            "from": "Countries",
-            "localField": "_id",
-            "foreignField": "match.country_id",
+            "from": "countries",
+            "localField": "match.country_id",
+            "foreignField": "_id",
             "as": "country"
         }
     },
     {
         "$match": {
             "team.team_name": "Portugal",
-            "tournament.tournament_name": "FIFA World Cup Qualification",
+            'tournament.tournament_name': 'FIFA World Cup qualification',
             "country.country_name": "Portugal"
         }
     },
-    {
+        {
         "$group": {
             "_id": "$scorer",
-            "total_gols": {"$sum": 1}
+            "total_gols": { "$sum": 1 }
         }
     },
     {
-        "$sort": {"total_gols": -1}
+        "$sort": { "total_gols": -1 } 
     },
     {
         "$limit": 5
     }
 ]
 
+
 result = goalscores.aggregate(complexQuery1)
+pprint.pprint(list(result))
 
+
+ #Complex Query 2
+complexQuery2 = [
+    {
+        "$lookup": {
+            "from": "teams",
+            "localField": "home_team_id",
+            "foreignField": "_id",
+            "as": "home_team"
+        }
+    },
+    {
+        "$lookup": {
+            "from": "teams",
+            "localField": "away_team_id",
+            "foreignField": "_id",
+            "as": "away_team"
+        }
+    },
+    {
+        "$lookup": {
+            "from": "Goalscorers",
+            "localField": "match_id",
+            "foreignField": "_id",
+            "as": "goalscorers"
+        }
+    },
+    {
+        "$group": {
+            "_id": {
+                "match_id": "$match_id",
+                "date": "$date",
+                "home_team": "$home_team.team_name",
+                "away_team": "$away_team.team_name",
+                "home_score": "$home_score",
+                "away_score": "$away_score"
+            },
+            "total_goals": {
+                "$sum": {
+                "$add": ["$home_score", "$away_score"]
+                }
+            }
+        }
+    },
+    {
+    "$sort": {
+      "total_goals": -1 
+    }
+    }, 
+    {
+        "$limit": 5
+    }
+]
+
+result2 = matches.aggregate(complexQuery2)
 print()
-print("Top 5 scorers from Portugal in FIFA World Cup Qualification:")
-for doc in result:
-    pprint.pprint(doc)
-
-# #Complex Query 2
-# complexQuery2 = [
-#     {
-#         "$lookup": {
-#             "from": "teams",
-#             "localField": "._id",
-#             "foreignField": "matches.home_team._id",
-#             "as": "home_team"
-#         }
-#     },
-#     {
-#         "$unwind": "$home_team"
-#     },
-#     {
-#         "$lookup": {
-#             "from": "teams",
-#             "localField": "._id",
-#             "foreignField": "matches.away_team._id",
-#             "as": "away_team"
-#         }
-#     },
-#     {
-#         "$unwind": "$away_team"
-#     },
-#     {
-#         "$lookup": {
-#             "from": "goalscorers",
-#             "localField": "._id",
-#             "foreignField": "match._id",
-#             "as": "goalscorers"
-#         }
-#     },
-#     {
-#         "$group": {
-#             "_id": {
-#                 "match_id": "$match_id",
-#                 "date": "$date",
-#                 "home_team": "$home_team.team_name",
-#                 "away_team": "$away_team.team_name",
-#                 "home_score": "$home_score",
-#                 "away_score": "$away_score"
-#             },
-#             "total_goals": {"$sum": {"$size": "$goalscorers"}}
-#         }
-#     },
-#     {
-#         "$sort": {"total_goals": -1}
-#     },
-#     {
-#         "$limit": 5
-#     }
-# ]
-
-# result2 = matches.aggregate(complexQuery2)
-# print()
-# print("Complex query 2:")
-# for doc in result2:
-#     pprint.pprint(doc)
+print("Complex query 2:")
+pprint.pprint(list(result2))
 
 # c. One update
 updateQuery = {'date':'1882-02-18'}
