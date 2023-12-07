0868374da647aa55a6235cd2314019f54c1ee30b
MiguelRamos1 <126819817+MiguelRamos1@users.noreply.github.com>
2023-12-07 20:29:29 +0000

    oi
    OP

---
+++ BDA_project/mongo_querys_op.py
@@ -0,0 +1,136 @@
+import time
+from pymongo import MongoClient
+import pprint
+
+client=MongoClient()
+client = MongoClient('localhost', 27017)
+
+db = client.BDA2324_4_OP
+
+goalscores = db.goalscores
+shootouts = db.shootouts
+teams = db.teams
+matches = db.matches
+countries = db.countries
+tournaments = db.tournaments
+
+with open('performance_mongo.txt', 'w') as querys_archive:
+    querys_archive.write("Simple Query1 BO, Simple Query2 BO, ComplexQuery1 BO, ComplexQuery2 BO, Simple Query1 AO, Simple Query2 AO, ComplexQuery1 AO, ComplexQuery2 AO\n")
+
+# a. Two simples queries, selecting data from one or two columns/fields
+
+#Query simples que irá obter todos os jogos que foram realizados em Lisbon, em 2023
+simpleQuery1 = { '$and': [ { 'city': "Lisbon" }, {'date': {"$regex": "2023"}} ] }
+
+start_time_simpleQuery1 = time.time()
+mydoc = matches.find(simpleQuery1)
+end_time_simpleQuery1 = time.time()
+
+print("Matches in Lisbon in 2023:", end='')
+counter=0
+for doc in mydoc:
+    pprint.pprint(doc)
+    counter=counter+1
+print(' ', counter, ' resultados')
+
+time_simpleQuery1 = end_time_simpleQuery1 - start_time_simpleQuery1
+print("---------------------------------------Tempo total da operação de SimpleQuery1:", time_simpleQuery1 , 'segundos')
+
+simpleQuery2 = {'$and':[{'scorer': 'Cristiano Ronaldo'}, {'minute': {'$lt': 5}}]}
+
+start_time_simpleQuery2 = time.time()
+mydoc = goalscores.find(simpleQuery2)
+end_time_simpleQuery2 = time.time()
+
+print("\nGolos que o Ronaldo marcou antes dos 5 minutos:", end='')
+counter=0
+for doc in mydoc:
+    pprint.pprint(doc)
+    counter=counter+1
+print(' ', counter, ' resultados')
+time_simpleQuery2 = end_time_simpleQuery2 - start_time_simpleQuery2
+print("---------------------------------------Tempo total da operação de SimpleQuery2:", time_simpleQuery2, 'segundos')
+
+# b. Two complex queries, using joins and aggregates, involving at least 2 tables/collections of your database (em que uma tem que ter mais do 5 joins)
+
+#COMPLEX QUERY1: Top 5 jogadores (scorer), de Portugal(team), que tem mais golos no torneio: FIFA World Cup Qualification, 
+# cujo os jogos foram realizados em Portugal 
+# e que esses jogos podem ter ido ou não a disputa de penaltys. 
+
+complexQuery1 = [
+    {
+        "$lookup": {
+            "from": "matches",
+            "localField": "match_id",
+            "foreignField": "_id",
+            "as": "match"
+        }
+    },
+    {
+        "$match": {
+            'match.tournament': 'FIFA World Cup qualification',
+            'match.country': 'Portugal',
+            'team': 'Portugal'
+        }
+    },
+        {
+        "$group": {
+            "_id": "$scorer",
+            "total_gols": { "$sum": 1 }
+        }
+    },
+    {
+        "$sort": { "total_gols": -1 } 
+    },
+    {
+        "$limit": 5
+    }
+]
+
+start_time_complexQuery1 = time.time()
+result = goalscores.aggregate(complexQuery1)
+end_time_complexQuery1 = time.time()
+
+print("\nResultado da complex query 1:")
+pprint.pprint(list(result))
+time_complexQuery1 = end_time_complexQuery1 - start_time_complexQuery1
+print("---------------------------------------Tempo total da operação de ComplexQuery1:", time_complexQuery1, 'segundos')
+
+#Complex Query 2
+complexQuery2 = [
+    
+    {"$lookup": {"from": "Goalscorers",
+                "localField": "match_ids",
+                "foreignField": "_id",
+                "as": "goalscorers"
+                }
+    },
+    {
+        "$group": {
+            "_id": "$_id",
+            "date": { "$first": "$date" },
+            "home_team": { "$first": "$home_team" },
+            "away_team": { "$first": "$away_team" },
+            "home_score": { "$first": "$home_score" },
+            "away_score": { "$first": "$away_score" },
+            "total_goals": {
+                "$sum": { "$add": ["$home_score", "$away_score"] }
+            }
+        }
+    },
+    {"$sort": {"total_goals": -1}}, 
+    {"$limit": 5}
+]
+
+start_time_complexQuery2 = time.time()
+result2 = matches.aggregate(complexQuery2)
+end_time_complexQuery2 = time.time()
+
+print("\nResultado da complex query 2:")
+pprint.pprint(list(result2))
+time_complexQuery2 = end_time_complexQuery2 - start_time_complexQuery2
+print("---------------------------------------Tempo total da operação de ComplexQuery2:", time_complexQuery2, 'segundos')
+with open('performance_mongo.txt', 'a') as querys_archive:
+    querys_archive.write(str(time_simpleQuery1) + ', ' + str(time_simpleQuery2) + ', ' + str(time_complexQuery1) + ', ' + str(time_complexQuery2))
+
+print("\nInserted new data")
