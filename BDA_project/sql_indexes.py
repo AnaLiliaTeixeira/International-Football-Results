cce9fdd97177b21d3ef11d9c557d7f2dfd9875d0
Daniel Lopes <danielgoncalo2002@gmail.com>
2023-12-08 11:16:47 +0000

    sql indexes

--- BDA_project/sql_indexes.py
+++ BDA_project/sql_indexes.py
@@ -10,6 +10,48 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
+# ---------------------INDEX 1-----------------------------
+query = "CREATE INDEX idx_city_date ON matches(city, date)"
+mycursor.execute(query)
+
+sqlShowIndexes = "show index from Matches"
+
+mycursor.execute(sqlShowIndexes)
+indexList = mycursor.fetchall()
+#---------------------INDEX 2-----------------------------
+query = "CREATE INDEX idx_home_away_team_id ON matches(home_team_id, away_team_id);"
+mycursor.execute(query)
+sqlShowIndexes = "show index from Matches"
+mycursor.execute(sqlShowIndexes)
+indexList = mycursor.fetchall()
+#---------------------INDEX 3-----------------------------
+query = "CREATE INDEX idx_scorer_minute ON Goalscorers(scorer, minute)"
+mycursor.execute(query)
+sqlShowIndexes = "show index from Goalscorers"
+mycursor.execute(sqlShowIndexes)
+indexList = mycursor.fetchall()
+# #---------------------INDEX 4-----------------------------
+query = "CREATE INDEX idx_team_scorer ON Goalscorers(team_id, scorer)"
+mycursor.execute(query)
+sqlShowIndexes = "show index from Goalscorers"
+mycursor.execute(sqlShowIndexes)
+indexList = mycursor.fetchall()
+
+# # CREATE INDEX idx_city_date ON Matches(city, date);
+# # CREATE INDEX idx_date ON Matches(date);
+# # CREATE INDEX idx_home_away_team_id ON Matches(home_team_id, away_team_id);
+# # CREATE INDEX idx_scorer_minute ON Goalscorers(scorer, minute);
+# # CREATE INDEX idx_match_id ON Goalscorers(match_id);
+# # CREATE INDEX idx_team_scorer ON Goalscorers(team_id, scorer);
+# # CREATE INDEX idx_match_id_shootouts ON Shootouts(match_id);
+# # CREATE INDEX idx_team_name ON Teams(team_name);
+# # CREATE INDEX idx_team_id ON Teams(team_id);
+# # CREATE INDEX idx_country_name ON Countries(country_name);
+# # CREATE INDEX idx_country_id ON Countries(country_id);
+# # CREATE INDEX idx_tournament_name ON Tournaments(tournament_name);
+# # CREATE INDEX idx_tournament_id ON Tournaments(tournament_id);
+
+
 print("----First Simple Query----")
 # a. Two simples queries, selecting data from one or two columns/fields
 # Query simples que irá obter todos os jogos que foram realizados em Lisbon, em 2023
@@ -124,35 +166,4 @@ print("--------------------------------------------Tempo  do Select após UPDATE
 
 total_time_update = end_time_select_after_update - start_time_select_before_update
 print("--------------------------------------------Tempo total das operações de UPDATE:", total_time_update)
-
-
-
-# Insert one
-start_time_insert = time.time()
-query = "INSERT INTO Teams (team_name) VALUES (%s);"
-mycursor.execute(query,["BDA2324_4_team1"])
-team1_id = mycursor.lastrowid
-mycursor.execute(query,["BDA2324_4_team2"])
-team2_id = mycursor.lastrowid
-query= """INSERT INTO tournaments (tournament_name) VALUES (%s);"""
-mycursor.execute(query,["BDA2324_4_tournament"])
-tournment_id = mycursor.lastrowid
-query = """INSERT INTO countries (country_name) VALUES (%s);"""
-mycursor.execute(query,['BDA2324_4_country'])
-country_id = mycursor.lastrowid
-query = "INSERT INTO Matches (date, home_team_id, away_team_id, home_score, away_score, tournament_id, city, country_id, neutral) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-data_to_insert = ('2023-11-30',team1_id,team2_id,1,1,tournment_id,'Lisbon',country_id,False)
-match_id = mycursor.execute(query,data_to_insert)
-query="INSERT INTO Goalscorers (match_id, team_id, scorer, minute, own_goal, penalty) VALUES (%s, %s, %s, %s, %s, %s)"
-data_to_insert = (match_id,team1_id,'Tomas Piteira',44,False,False)
-mycursor.execute(query,data_to_insert)
-data_to_insert = (match_id,team2_id,'Daniel Lopes',44,False,False)
-mycursor.execute(query,data_to_insert)
-query = "INSERT INTO Shootouts (match_id, winner_id, first_shooter_id) VALUES (%s, %s, %s)"
-data_to_insert = (match_id,team2_id,team1_id)
-mycursor.execute(query,data_to_insert)
-print("-----------INSERT-----------")
-end_time_insert = time.time()
-print("--------------------------------------------Tempo de todos os INSERT:", end_time_insert - start_time_insert)
-mydb.commit()
 
